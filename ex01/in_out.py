aadebc3b2d39482e2a97f98a88b27e5f3281678a
Hugo Fragnoli <hugofragnoli@gmail.com>
2026-02-13 19:02:02 +0100

    try 3

--- ex01/in_out.py
+++ ex01/in_out.py
@@ -7,8 +7,9 @@ def pow(x: int | float) -> int | float:
 def outer(x: int | float, function) -> object:
     count = x
     def inner() -> int | float:
-        nonlocal count # Permet de modifier la variable de la fonction outer
+        nonlocal count
+        # Permet de modifier la variable de la fonction outer
         # On applique la fonction passée en argument à 'count'
         count = function(count)
         return count
-    
+    return inner
