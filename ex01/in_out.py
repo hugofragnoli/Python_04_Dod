5ba2e9772ddb1000f6d1aa20206362133c7a2f73
Hugo Fragnoli <hugofragnoli@gmail.com>
2026-02-13 18:42:07 +0100

    debut ex01

---
+++ ex01/in_out.py
@@ -0,0 +1,11 @@
+def square(x: int | float) -> int | float:
+    x_square = x * x
+    return x_square
+
+def pow(x: int | float) -> int | float:
+    x_pow = x ** x
+    return x_pow
+
+def outer(x: int | float, function) -> object:
+    
+    return
