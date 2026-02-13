f36b27a77e30175273b6e46b2898fd11624c1a4c
Hugo Fragnoli <hugofragnoli@gmail.com>
2026-02-13 17:24:49 +0100

    try quartile, median et mean

--- ex00/statistics.py
+++ ex00/statistics.py
@@ -5,18 +5,27 @@ def ft_statistics(*args: any, **kwargs: any) -> None:
         nb_digits = len(args)
         list_methods = ["std, quartile, mean, median, var"]
         for key, value in kwargs.items():
-            if value in ["mean"]:
+            if value == "mean":
                 mean = sum(args) / nb_digits
-            elif value in "quartile":
-                quartile = sum(args)
-            elif value in "quartile":
-                quartile = sum(args)
-                
-                
-        
-
-        print(list_args)
-        print(list_kwargs)
+                print(f"mean : {mean}")
+            elif value == "quartile":
+                first_quar = nb_digits // 4
+                last_quar = nb_digits // (3/4)
+                print(f"quartile : [{args[first_quar]}, {args[last_quar]}]")
+            elif value == "median":
+                sort = sorted(args)
+                if nb_digits % 2 == 0:
+                    median = (sort[nb_digits // 2 - 1] + sort[nb_digits // 2]) / 2
+                else:
+                    median = sort[nb_digits // 2]
+                print(f"median : {median}")
+            # elif value == "std":
+            #     std = sum(args)
+            # elif value == "var":
+            #     var = 
+            else:
+                print("ERROR")
         return
 
     except:
+        
