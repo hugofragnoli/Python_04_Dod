356f91572013757db3ebf462905f879a1ca0bf6d
Hugo Fragnoli <hugofragnoli@gmail.com>
2026-02-14 23:14:57 +0100

    ok javais mis de la merde dans le tester -> mon list_fields marche bien

--- ex03/new_student.py
+++ ex03/new_student.py
@@ -16,10 +16,9 @@ class Student:
         def __post_init__(self):
             """Cette méthode remplace la logique de __init__"""
             self.id = generate_id()
-            self.login = f"{self.name[0].upper}{self.surname}"
+            self.login = f"{self.name[0]}{self.surname}"
 
 def new_stud_in_town(*args, **kwargs):
-    students = []
     list_fields = ["name", "surname", "active"]
     for key in kwargs:
         if key not in list_fields:
